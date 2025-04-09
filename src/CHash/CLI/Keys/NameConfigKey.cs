b96400c216493f585aeca758739f7c3cd14bf2e0
Dmitry <truckcyber@yandex.ru>
2025-04-09 21:14:50 +0300

    Key classes are done

---
+++ src/CHash/CLI/Keys/NameConfigKey.cs
@@ -0,0 +1,15 @@
+﻿using CLI.Abstractions;
+using CLI.Enums;
+
+namespace CLI.Keys;
+
+public class NameConfigKey: ConfigKey
+{
+    public NameConfigKey()
+    {
+        ShortName = "-n";
+        KeyName = CommandKey.Name;
+        ValueIsRequired = true;
+        ExpectedValue = "string";
+    }
+}
